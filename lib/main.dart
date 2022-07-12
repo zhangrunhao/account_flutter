00f17e306520b30c3d03a6b50bf6dcfce35c9695
zhangrh <runhaozhang@sohu-inc.com>
2022-07-12 13:32:43 +0800

    feat: accountl select and local

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'package:account_flutter/pages/home/home_page.dart';
 import 'package:account_flutter/pages/trade/trade_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+         Locale('zh')
+       ],
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/pages/trade/tools_list.dart
+++ lib/pages/trade/tools_list.dart
@@ -1,7 +1,60 @@
 import 'package:flutter/material.dart';
 
-class ToolsList extends StatelessWidget {
+class ToolsList extends StatefulWidget {
   const ToolsList({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _ToolsListState();
+}
+
+class _ToolsListState extends State<ToolsList> {
+  DateTime selectDate = DateTime.now();
+  String accountName = "选择账户";
+
+  Future<void> _selectDate(BuildContext content) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      locale: const Locale("zh", "ZH"),
+      initialDate: selectDate,
+      firstDate: DateTime(2000, 01),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != selectDate) {
+      setState(() {
+        selectDate = picked;
+      });
+    }
+  }
+
+  Future<int?> _selectAccount(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return ListView(
+            children: [
+              ListTile(
+                leading: const Icon(Icons.abc_outlined),
+                title: const Text("微信"),
+                onTap: () {
+                  setState(() {
+                    accountName = "微信";
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.payment),
+                title: const Text("支付宝"),
+                onTap: () {
+                  setState(() {
+                    accountName = "支付宝";
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +63,41 @@ class ToolsList extends StatelessWidget {
       height: 40,
       child: ListView(
         scrollDirection: Axis.horizontal,
-        children: _buildToolBtn(10),
+        children: [
+          _buildDatePickerButton(
+            _selectDate,
+            context,
+            '${selectDate.year.toString()}-${selectDate.month.toString()}-${selectDate.day.toString()}',
+          ),
+          _buildAccountPickerButton(
+            _selectAccount,
+            context,
+            accountName,
+          ),
+        ],
       ),
     );
   }
 }
 
-List<Widget> _buildToolBtn(int count) {
-  return List.generate(
-    count,
-    (index) => Container(
-      margin: const EdgeInsets.all(5),
-      child: ElevatedButton(onPressed: () => {}, child: const Text("时间")),
+Widget _buildDatePickerButton(
+    Function pressCallback, BuildContext context, String buttonTitle) {
+  return Container(
+    margin: const EdgeInsets.all(5),
+    child: ElevatedButton(
+      onPressed: () => {pressCallback(context)},
+      child: Text(buttonTitle),
+    ),
+  );
+}
+
+Widget _buildAccountPickerButton(
+    Function pressCallback, BuildContext context, String buttonTitle) {
+  return Container(
+    margin: const EdgeInsets.all(5),
+    child: ElevatedButton(
+      onPressed: () => {pressCallback(context)},
+      child: Text(buttonTitle),
     ),
   );
 }
